bb63befb7efda3e91b5cd30ceb962d063953b872
Heather Hartley <hlhartley@gmail.com>
2019-03-24 11:28:31 -0600

    Adds header components

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Header from '../../components/Header/Header';
 
 class App extends Component {
   render() {
@@ -6,6 +7,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           App
+          <Header />
         </header>
       </div>
     );
