f647e91576d4ff14f71c6ac753fb7d67b2a8c954
Heather Hartley <hlhartley@gmail.com>
2019-03-24 15:47:45 -0600

    Adds section positioning with flex for components

--- src/components/ParentSection/ParentSection.js
+++ src/components/ParentSection/ParentSection.js
@@ -5,8 +5,14 @@ import ProjectSection from '../../components/ProjectSection/ProjectSection';
 export const ParentSection = () => {
     return (
         <div className="ParentSection">
-            <PaletteSection/>
-            <ProjectSection/>
+            <div className="section-headers">
+                <h2>PALETTE 1</h2>
+                <h2>PROJECT 1</h2>
+            </div>
+            <div className="section-container">
+                <PaletteSection/>
+                <ProjectSection/>
+            </div>
         </div>
     )
 }
