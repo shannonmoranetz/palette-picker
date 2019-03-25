52bdce2f5795055618041eb20362c4e384b08639
shannon <shannonrmoranetz@gmail.com>
2019-03-24 19:18:55 -0600

    Add project section elements

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEdit } from '@fortawesome/free-solid-svg-icons';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
 export class ProjectCard extends Component {
   render() {
     return (
       <div className="ProjectCard">
-        <p>ProjectCard</p>
+        <p className="project-name">Project Name</p>
+          <div className="palette-container">
+            <p className="palette-name">Palette Name</p>
+              <div className="palette-minibox">colors</div>
+              <button className="load-button"><FontAwesomeIcon icon={faEdit} className="load-icon"/></button>
+              <button className="delete-button"><FontAwesomeIcon icon={faTrash} className="deelete-icon"/></button>
+          </div>
       </div>
     );
   }
--- src/containers/ProjectList/ProjectList.js
+++ src/containers/ProjectList/ProjectList.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import ProjectCard from '../../components/ProjectCard/ProjectCard';
+import uuid from 'uuid/v4';
 
 export class ProjectList extends Component {
   render() {
     return (
       <div className="ProjectList">
-        <p>ProjectList</p>
-        <ProjectCard/>
+        <p className="projectlist-subtitle">Your Creations:</p>
+        {
+          this.props.projects.map(project => {
+            return <ProjectCard key={uuid()} />
+          })
+        }
       </div>
     );
   }
 }
 
-export default ProjectList;
+export const mapStateToProps = (state) => ({
+  projects: state.projects
+});
+
+export default connect(mapStateToProps)(ProjectList);
