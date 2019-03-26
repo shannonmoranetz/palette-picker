b923084dabb5bd69e3e9d28801b4e4cc2d8d7eba
Heather Hartley <hlhartley@gmail.com>
2019-03-26 16:57:14 -0600

    Fixes toggleColorLock function and adds remove_locked_hexcode action and reducer

--- src/containers/ColorCard/ColorCard.js
+++ src/containers/ColorCard/ColorCard.js
@@ -3,42 +3,43 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLock } from '@fortawesome/free-solid-svg-icons';
 import { faUnlock } from '@fortawesome/free-solid-svg-icons';
 import { connect } from 'react-redux';
-import { setLockedHexcodes } from '../../actions/index';
+import { setLockedHexcodes, removeLockedHexcode } from '../../actions';
 
 export class ColorCard extends Component {
   constructor() {
     super()
     this.state = {
-      icon: faUnlock,
+      icon: faUnlock
     }
   }
-  toggleColorLock = () => {
-    if (!this.props.lockedHexcodes.length) {
-      this.props.setLockedHexcodes(this.props.color)
+
+  toggleColorLock = async () => {
+    if (!this.props.lockedHexcodes.includes(this.props.color)) {
+      await this.props.setLockedHexcodes(this.props.color)
       this.setState({ icon: faLock })
     } else {
+      await this.props.removeLockedHexcode(this.props.color)
       this.setState({ icon: faUnlock })
     }
   }
   
   render() {
-    // console.log(this.props.color)
     return (
       <div className="ColorCard" style={{backgroundColor: `#${this.props.color}`}}>
-            <FontAwesomeIcon icon={this.state.icon} className="lock-icon" onClick={() => this.toggleColorLock()}/>
+            <FontAwesomeIcon icon={this.state.icon === faLock ? faLock : faUnlock} className="lock-icon" onClick={() => this.toggleColorLock()}/>
             <p className="hexcode">#{this.props.color}</p>
       </div>
     );
   }
 }
 
-
 export const mapStateToProps = (state) => ({
-	lockedHexcodes: state.randomHexcodes
+	lockedHexcodes: state.lockedHexcodes
 });
 
 export const mapDispatchToProps = (dispatch) => ({
-  setLockedHexcodes: () => dispatch(setLockedHexcodes()),
+  setLockedHexcodes: (hexcode) => dispatch(setLockedHexcodes(hexcode)),
+  removeLockedHexcode: (hexcode) => dispatch(removeLockedHexcode(hexcode))
 });
 
 
--- src/reducers/lockedHexcodesReducer.js
+++ src/reducers/lockedHexcodesReducer.js
@@ -1,7 +1,20 @@
 export const lockedHexcodesReducer = (state = [], action) => {
     switch (action.type) {
-        case 'SET_RANDOM_HEXCODES':
-            return [...state, action.hexcode];
+        case 'SET_LOCKED_HEXCODES':
+            if (!state.includes(action.hexcode)) {
+                return [...state, action.hexcode];
+            } else {
+                return state;
+            }
+        case 'REMOVE_LOCKED_HEXCODE':
+            if (state.includes(action.hexcode)) {
+                const lockedHexcodes = state;
+                const lockedHexcodeIndex = lockedHexcodes.indexOf(action.hexcode);
+                lockedHexcodes.splice(lockedHexcodeIndex, 1);
+                return lockedHexcodes;
+            } else {
+                return state;
+            }
         default:
             return state;
     }
