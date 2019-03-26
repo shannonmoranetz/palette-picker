c63eec89fdc34ddaed476e829d16c211fcabab87
shannon <shannonrmoranetz@gmail.com>
2019-03-25 21:33:08 -0600

    Clean up render method on projectcard

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -3,13 +3,9 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEdit } from '@fortawesome/free-solid-svg-icons';
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
 import { connect } from 'react-redux';
+import uuid from 'uuid/v4';
 
 export class ProjectCard extends Component {
-  constructor() {
-    super();
-    this.state = {
-    }
-  }
 
 	findProjectPalette = () => {
     const  { palettes, project } = this.props;
@@ -23,14 +19,16 @@ export class ProjectCard extends Component {
     let matchingPalettes = this.findProjectPalette();
     return matchingPalettes.map((palette) => {
       const { name, color1, color2, color3, color4, color5 } = palette;
-          return <div>
+        return (
+          <div key={uuid()} className="mapped-palettes">
             <p className="palette-name">{name}</p>
-            <p>color1: {color1} </p>
-            <p>color2: {color2} </p>
-            <p>color3: {color3} </p>
-            <p>color4: {color4} </p>
-            <p>color5: {color5} </p> 
+            <p className="color-minibox">color1: {color1}</p>
+            <p className="color-minibox">color2: {color2}</p>
+            <p className="color-minibox">color3: {color3}</p>
+            <p className="color-minibox">color4: {color4}</p>
+            <p className="color-minibox">color5: {color5}</p>
           </div>
+        )
       })
   }
 
@@ -42,11 +40,9 @@ export class ProjectCard extends Component {
           <div>
             <p className="project-name">{project.name}</p>
               <div className="palette-container">
-                  <div className="palette-minibox">
-                    { this.returnElements() }
-                  </div> 
+                  { this.returnElements() }
                   <button className="load-button"><FontAwesomeIcon icon={faEdit} className="load-icon"/></button>
-                  <button className="delete-button"><FontAwesomeIcon icon={faTrash} className="deelete-icon"/></button>
+                  <button className="delete-button"><FontAwesomeIcon icon={faTrash} className="delete-icon"/></button>
               </div>
             <button className="delete-project-button">Delete Project</button>
           </div>
