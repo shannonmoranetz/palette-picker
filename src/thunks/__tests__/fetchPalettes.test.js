ba8a737048185c3046274c016b569a0d0b6f299b
Heather Hartley <hlhartley@gmail.com>
2019-03-28 00:40:28 -0600

    Finishes testing for fetchPalettes and fetchProjects thunks

--- src/thunks/__tests__/fetchPalettes.test.js
+++ src/thunks/__tests__/fetchPalettes.test.js
@@ -1,6 +1,9 @@
 import { fetchPalettes } from '../fetchPalettes';
 import { fetchData } from '../../utils/api';
 import * as actions from '../../actions';
+import { mockPalettes } from '../../__fixtures__/mockData';
+
+jest.mock('../../utils/api')
 
 describe('fetchPalettes', () => {
     let id;
@@ -13,8 +16,20 @@ describe('fetchPalettes', () => {
         mockDispatch = jest.fn()
     });
 
+    it('should call fetchData with the correct params', async () => {
+        await thunk(mockDispatch)
+        expect(fetchData).toHaveBeenCalledWith(`/projects/${id}/palettes`, 'GET')
+    });
+
+    it('should call dispatch with setPalettes if the response is ok', async () => {
+        const expected = mockPalettes;
+        fetchData.mockImplementation(() => expected)
+        await thunk(mockDispatch)
+        expect(mockDispatch).toHaveBeenCalledWith(actions.setPalettes(expected))
+    });
+
     it('should dispatch setError with a message if the response is not ok', async () => {
-        window.fetch = jest.fn().mockImplementation(() => {
+        fetchData.mockImplementation(() => {
             throw 'Error on fetching data'
         })
         await thunk(mockDispatch)
--- src/thunks/__tests__/fetchProjects.test.js
+++ src/thunks/__tests__/fetchProjects.test.js
@@ -1,20 +1,33 @@
 import { fetchProjects } from '../fetchProjects';
 import { fetchData } from '../../utils/api';
 import * as actions from '../../actions';
+import { mockProjects } from '../../__fixtures__/mockData';
+
+jest.mock('../../utils/api')
 
 describe('fetchProjects', () => {
-    let id;
     let mockDispatch;
     let thunk;
 
     beforeEach(() => {
-        id = 1
         thunk = fetchProjects()
         mockDispatch = jest.fn()
     });
 
+    it('should call fetchData with the correct params', async () => {
+        await thunk(mockDispatch)
+        expect(fetchData).toHaveBeenCalledWith(`/projects`, 'GET')
+    });
+
+    it('should call dispatch with setPalettes if the response is ok', async () => {
+        const expected = mockProjects;
+        fetchData.mockImplementation(() => expected)
+        await thunk(mockDispatch)
+        expect(mockDispatch).toHaveBeenCalledWith(actions.setProjects(expected))
+    });
+
     it('should dispatch setError with a message if the response is not ok', async () => {
-        window.fetch = jest.fn().mockImplementation(() => {
+        fetchData.mockImplementation(() => {
             throw 'Error on fetching data'
         })
         
