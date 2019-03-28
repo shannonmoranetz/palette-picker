f8601ca1adf0ba3f573403afb0af35efc4731688
Heather Hartley <hlhartley@gmail.com>
2019-03-27 22:00:46 -0600

    Add fetchPalettes and fetchProjects thunk tests

---
+++ src/thunks/__tests__/fetchProjects.test.js
@@ -0,0 +1,25 @@
+import { fetchProjects } from '../fetchProjects';
+import { fetchData } from '../../utils/api';
+import * as actions from '../../actions';
+
+describe('fetchProjects', () => {
+    let id;
+    let mockDispatch;
+    let thunk;
+
+    beforeEach(() => {
+        id = 1
+        thunk = fetchProjects()
+        mockDispatch = jest.fn()
+    });
+
+    it('should dispatch setError with a message if the response is not ok', async () => {
+        window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+            ok: false,
+            error: 'Error fetching data'
+        }))
+        
+        await thunk(mockDispatch)
+        expect(mockDispatch).toHaveBeenCalledWith(actions.setError('Error fetching data'))
+    });
+});
